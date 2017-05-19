2997c83609784230f7f2f06de32fece133aa044f
lzhao <zhao434@usc.edu>
2017-05-19 11:14:06 -0700

    new product detail component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,7 +5,8 @@ import { StarsComponent } from './stars/stars.component';
 import { SearchComponent } from './search/search.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { HomeComponent } from './home.component';
-import { ProductDetailComponent } from './product-list/product-detail/product-detail.component';
+import { ProductItemComponent } from './product-list/product-item/product-item.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 @NgModule({
   imports: [
@@ -17,6 +18,7 @@ import { ProductDetailComponent } from './product-list/product-detail/product-de
     SearchComponent,
     CarouselComponent,
     HomeComponent,
+    ProductItemComponent,
     ProductDetailComponent
   ],
   exports: [
--- src/app/home/product-list/product-item/product-item.component.ts
+++ src/app/home/product-list/product-item/product-item.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Product } from 'app/shared/product.model';
 
 @Component({
-  selector: 'app-product-detail',
-  templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.scss']
+  selector: 'app-product-item',
+  templateUrl: './product-item.component.html',
+  styleUrls: ['./product-item.component.scss']
 })
-export class ProductDetailComponent implements OnInit {
+export class ProductItemComponent implements OnInit {
 
   @Input()
   product: Product;
